01842ceb851b2c3c0a136c5698dba38cdf0c6017
Allie Wehrman <allie.wehrman@gmail.com>
2024-10-18 16:10:48 -0500

    extend note model

--- apps/queue/src/queue.ts
+++ apps/queue/src/queue.ts
@@ -1,15 +1,33 @@
 import { Queue, Worker } from "bullmq";
 import { createNote, Note } from "database";
 import { load } from "cheerio";
-import { ParsedHTMLFile } from "./types";
+import {
+  ParsedHTMLFile,
+  ParsedIngredientLine,
+  ParsedInstructionLine,
+} from "./types";
+import { parseISO } from "date-fns";
 
 export const parseHTML = (note: string): ParsedHTMLFile => {
   const $ = load(note);
   const enNote = $("en-note");
   // get the content from meta itemprop="title"
   const title = $('meta[itemprop="title"]').attr("content");
+
   if (title === undefined) {
     throw new Error("This file doesn't have a title!");
+  }
+
+  const historicalCreatedAtString = $('meta[itemprop="created"]').attr(
+    "content"
+  );
+  let historicalCreatedAt: Date | undefined;
+
+  if (historicalCreatedAtString) {
+    historicalCreatedAt = parseISO(historicalCreatedAtString);
+    if (isNaN(historicalCreatedAt.getTime())) {
+      throw new Error("Invalid date format in 'created' meta tag");
+    }
   }
 
   // get the content from meta itemprop="sourceurl"
@@ -46,22 +64,27 @@ export const parseHTML = (note: string): ParsedHTMLFile => {
 
   // go through our contents array, and sort these string into ingredients vs instructions based on where we see <br> elements
   const ingredients: string[][] = [];
-  const instructions: string[] = [];
+  const instructions: ParsedInstructionLine[] = [];
   let currentChunk: string[] = [];
+  let blockIndex = 0;
 
-  contents.forEach((line, index) => {
+  contents.forEach((line, lineIndex) => {
     if (line === "<br>") {
       if (currentChunk.length > 0) {
         ingredients.push(currentChunk);
         currentChunk = [];
+        blockIndex++;
       }
     } else if (
       line.length > 0 &&
-      index > 0 &&
-      contents[index - 1] === "<br>" &&
-      contents[index + 1] === "<br>"
+      lineIndex > 0 &&
+      contents[lineIndex - 1] === "<br>" &&
+      contents[lineIndex + 1] === "<br>"
     ) {
-      instructions.push(line);
+      instructions.push({
+        reference: line,
+        lineIndex,
+      });
     } else if (line.length > 0) {
       currentChunk.push(line);
     }
@@ -71,14 +94,25 @@ export const parseHTML = (note: string): ParsedHTMLFile => {
     ingredients.push(currentChunk);
   }
 
+  const parsedIngredients: ParsedIngredientLine[] = ingredients.flatMap(
+    (block, blockIndex) =>
+      block.map((line, lineIndex) => ({
+        blockIndex,
+        lineIndex,
+        reference: line,
+        isParsed: false,
+      }))
+  );
+
   const contentsString = contents.join("\n");
 
   console.log(`Finished parsing "${title}."`);
 
   return {
     title,
+    historicalCreatedAt,
     sourceUrl,
-    ingredients,
+    ingredients: parsedIngredients,
     instructions,
     contents: contentsString,
     // image
--- apps/queue/src/types.ts
+++ apps/queue/src/types.ts
@@ -1,8 +1,22 @@
+export type ParsedIngredientLine = {
+  blockIndex: number;
+  lineIndex: number;
+  reference: string;
+  isParsed: boolean;
+  parsedAt?: Date;
+};
+
+export type ParsedInstructionLine = {
+  lineIndex: number;
+  reference: string;
+};
+
 export type ParsedHTMLFile = {
   title: string;
+  historicalCreatedAt?: Date;
   contents: string;
-  ingredients: string[][];
-  instructions: string[];
+  ingredients: ParsedIngredientLine[];
+  instructions: ParsedInstructionLine[];
   sourceUrl?: string;
   image?: string;
 };
