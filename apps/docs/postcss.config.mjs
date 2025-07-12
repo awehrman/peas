d9bfe3e0673ee618c2980670d8cdf0fa12d9b0d8
Alexandra Wehrman <allie.wehrman@gmail.com>
2025-07-12 10:02:43 -0500

    cleanup

--- apps/docs/postcss.config.mjs
+++
@@ -1,3 +0,0 @@
-import { postcssConfig } from "@peas/tailwind/postcss";
-
-export default postcssConfig;
--- apps/playground/next.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  transpilePackages: ["@peas/ui", "@peas/theme", "@peas/tailwind"],
-  reactStrictMode: true,
-};
-
-export default nextConfig; 
